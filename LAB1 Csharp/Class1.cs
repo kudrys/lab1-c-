e78de281b0dab6c9dfae1a75d29d539eea6daf0f
Kudrys <ryskuduk@gmail.com>
2017-05-08 14:39:31 +0200

    pierwszy komit

---
+++ LAB1 Csharp/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1_Csharp
+{
+    class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("kek");
+            Console.WriteLine("{1} pierwszy arg to: {0}", args[0] , ":)");
+            while(true) { }
+        }
+    }
+}
