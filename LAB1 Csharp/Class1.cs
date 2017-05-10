9497816f914a929e2bbdeca141416939a6fe55bb
Kudrys <ryskuduk@gmail.com>
2017-05-10 12:02:40 +0200

    serial i deserial

--- LAB1 Csharp/Class1.cs
+++ LAB1 Csharp/Class1.cs
@@ -2,31 +2,61 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
 
-namespace LAB1_Csharp
-{
-    class Class1
-    {
-        public static void Main(string[] args)
-        {
+namespace LAB1_Csharp{
+    class Class1{
+        public static void Main(string[] args){
             Console.WriteLine("kek");
             Console.WriteLine("{1} pierwszy arg to: {0}", args[0] , ":)");
             //-------------foldery
             DirectoryInfo di = new DirectoryInfo(@"D:\");
             DirectoryInfo[] dirTab = di.GetDirectories();
-            foreach (var singleDirectory in dirTab)
-            {
+            foreach (var singleDirectory in dirTab){
                 Console.WriteLine("singleDir: {0}", singleDirectory.Name);
 
             }
             //------------------poj pliki
-            foreach (var singleFile in di.GetFiles())
-            {
+            foreach (var singleFile in di.GetFiles()){
                 Console.WriteLine("singleFile: {0}", singleFile.Name);
             }
+            //-----------------Serializacja i deserializacja kolekcji
+            //-----------------Serializacja:
+            FileStream plikZSerialami = new FileStream("Serials.txt", FileMode.Create);
+            BinaryFormatter formater = new BinaryFormatter();
+
+            try{
+                formater.Serialize(plikZSerialami, dirTab);
+            }
+            catch{
+                Console.WriteLine("ERROR");
+            }
+            finally{
+                plikZSerialami.Close();
+            }
+            //------------------Deserializacja:
+            FileStream OtwieralnyPlik = new FileStream("Serials.txt", FileMode.Open);
+            OtwieralnyPlik.Position = 0;
+            DirectoryInfo[] de = null;
+            BinaryFormatter deformater = new BinaryFormatter();
+            de = (DirectoryInfo[]) deformater.Deserialize(OtwieralnyPlik);
+            
+            try {
+                foreach (var xex in de) {
+                    Console.WriteLine("deserializowane: {0}", xex);
+                }
+            }catch(SerializationException e) {
+                Console.WriteLine("ERROR DESERIALIZE: {0}", e.Message);
+            } finally {
+                OtwieralnyPlik.Close();
+            }
+
+
+
 
 
 
@@ -37,17 +67,13 @@ namespace LAB1_Csharp
     }
 
 
-class Test
-    {
-        public static void Kain()
-        {
+class Test{
+        public static void Kain(){
             // Specify the directories you want to manipulate.
             DirectoryInfo di = new DirectoryInfo(@"D:\MyDir");
-            try
-            {
+            try{
                 // Determine whether the directory exists.
-                if (di.Exists)
-                {
+                if (di.Exists){
                     // Indicate that the directory already exists.
                     Console.WriteLine("That path exists already.");
                     return;
@@ -62,8 +88,7 @@ class Test
                 //Console.WriteLine("The directory was deleted successfully.");
 
             }
-            catch (Exception e)
-            {
+            catch (Exception e){
                 Console.WriteLine("The process failed: {0}", e.ToString());
             }
             finally { }
